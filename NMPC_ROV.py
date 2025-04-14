8f49af125d9e2465bd694e6d9dee77b746ad541f
larsthomash <115690823+larsthomash@users.noreply.github.com>
2025-04-14 09:10:13 +0200

    Update NMPC_ROV.py

--- NMPC_ROV.py
+++ NMPC_ROV.py
@@ -1,14 +1,104 @@
 import casadi as ca
 import numpy as np
 
-class MPCParameters:
+
+class MPCTrajectory:
+    def __init__(self):
+        self.x = []
+        self.y = []
+        self.z = []
+        self.psi = []
+
+        self.resolution = 0.005  # A waypoint per X [m]
+
+        self.n = 4  # Amount of turns in helix
+        self.R = 1  # Radius for helix
+        self.h = 2  # Height for helix
+
+
+    def updateToWaypoints(self, initState, TrajVersion):
+        self.x = []
+        self.y = []
+        self.z = []
+        self.psi = []
+        displacements = []
+
+        pos0 = np.array([float(initState[0]),float(initState[1]),float(initState[2])])
+        if TrajVersion == 0:  # Lawnmover pattern
+            displacements = [
+                np.array([4.0, 0.0, 0.0]),
+                np.array([4.0, 2.0, 0.0]),
+                np.array([0.0, 2.0, 0.0]),
+                np.array([0.0, 4.0, 0.0]),
+                np.array([4.0, 4.0, 0.0]),
+                np.array([4.0, 6.0, 0.0]),
+                np.array([0.0, 6.0, 0.0]),
+            ]
+
+        if TrajVersion == 1:  # Sawtooth
+            displacements = [
+                np.array([1, 1, 0.0]),
+                np.array([2, -1, 0.0]),
+                np.array([3, 1, 0.0]),
+                np.array([4, -1, 0.0]),
+                np.array([5, 1, 0.0]),
+                np.array([6, -1, 0.0]),
+                np.array([7, 1, 0.0]),
+            ]
+
+        WP = [pos0]
+        for disp in displacements:
+            WP.append(pos0 + disp)
+
+        for i in range(len(WP)-1): # Calculates the length of each segment
+            x_i, y_i, z_i = WP[i]
+            x_e, y_e, z_e = WP[i+1]
+
+            distance = ((x_e - x_i)**2 + (y_e - y_i)**2 + (z_e - z_i)**2) ** 0.5
+            steps = int(np.ceil(distance / self.resolution))
+
+            for j in range(steps + 1):  # Creates a point at each cm
+
+                if i > 0 and j == 0:  # Avoid the same point twice
+                    continue
+                else:
+                    k = j / steps
+                    self.x.append(x_i + k * (x_e - x_i))
+                    self.y.append(y_i + k * (y_e - y_i))
+                    self.z.append(z_i + k * (z_e - z_i))
+                    self.psi.append(np.atan2(y_e-y_i, x_e-x_i))
+
+    def updateToHelix(self, initState):
+        self.x = []
+        self.y = []
+        self.z = []
+        self.psi = []
+
+
+        distance = ( (2 * np.pi * self.n * self.R)**2 + self.h**2 ) ** 0.5
+        steps = int(np.ceil(distance / self.resolution))
+
+        for i in range(steps + 1):  # Creates a point at each cm
+            k = 2*np.pi*self.n * i / steps
+            self.x.append(float(initState[0]) + self.R * np.cos(k))
+            self.y.append(float(initState[1]) + self.R * np.sin(k))
+            self.z.append(float(initState[2]) + (self.h / (2*np.pi*self.n)) * k)
+
+        for i in range(steps):
+            self.psi.append(np.atan2(self.y[i+1] - self.y[i], self.x[i+1] - self.x[i]))
+        self.psi.append(self.psi[-1]) # Add the last point
+
+
+class MPCParameters_:
     def __init__(self,
                  Q: ca.DM = None,
                  R: ca.DM = None,
                  N: int = 20,
                  stepHorizon: float = 0.05,
+                 desiredVelocity: float = 0.1,
+                 tau: float = 0.05,
                  u_max: float = 0.5, v_max: float = 0.5, w_max: float = 0.5, r_max: float = 0.5,
-                 u_ROC_max: float = 0.05, v_ROC_max: float = 0.05, w_ROC_max: float = 0.05, r_ROC_max: float = 0.05):
+                 u_ROC_max: float = 0.5, v_ROC_max: float = 0.5, w_ROC_max: float = 0.5, r_ROC_max: float = 0.5):
         """
         Args:
             Q: State error weight matrix.
@@ -28,37 +118,33 @@ class MPCParameters:
         self.R = R
         self.N = N
         self.stepHorizon = stepHorizon
+        self.desiredVelocity = desiredVelocity
+        self.tau = tau
 
         self.u_max = u_max
-        self.u_min = -u_max
         self.v_max = v_max
-        self.v_min = -v_max
         self.w_max = w_max
-        self.w_min = -w_max
         self.r_max = r_max
-        self.r_min = -r_max
 
         self.u_ROC_max = u_ROC_max
-        self.u_ROC_min = -u_ROC_max
         self.v_ROC_max = v_ROC_max
-        self.v_ROC_min = -v_ROC_max
         self.w_ROC_max = w_ROC_max
-        self.w_ROC_min = -w_ROC_max
         self.r_ROC_max = r_ROC_max
-        self.r_ROC_min = -r_ROC_max
+
+        self.manualMode = 1
+        self.activeTrajectory = 0
+        self.activeTarget = 0
+        self.whichTrajectory = 0
 
         self.updateParams = 0
 
-class MPCController:
-    def __init__(self, params: MPCParameters):
+class MPCController_:
+    def __init__(self, params: MPCParameters_):
         """
         Initialize the controller controller.
         Args:
             params: controller Parameters.
         """
-
-        self.target = ca.DM([0, 0, 0, 0])
-        self.state = ca.DM([0, 0, 0, 0])
 
         self.n_states = 4
         self.n_controls = 4
@@ -68,35 +154,70 @@ class MPCController:
         self.u0 = ca.DM.zeros(self.n_controls, self.N)
         self.u_prev = ca.DM.zeros(self.n_controls, 1)
 
+        self.target = ca.DM([0, 0, 0, 0])
+        self.state = ca.DM([0, 0, 0, 0])
+        self.trajectory = ca.DM.zeros(self.n_states, self.N)
+        self.elapsed_time_trajectory = 0
 
 
-    def update_from_params(self, params: MPCParameters):
+    def update_from_params(self, params: MPCParameters_):
         self.Q = params.Q
         self.R = params.R
         self.N = params.N
         self.stepHorizon = params.stepHorizon
+        self.desiredVelocity = params.desiredVelocity
+        self.tau = params.tau
 
         self.u_max = params.u_max
-        self.u_min = params.u_min
+        self.u_min = -params.u_max
         self.v_max = params.v_max
-        self.v_min = params.v_min
+        self.v_min = -params.v_max
         self.w_max = params.w_max
-        self.w_min = params.w_min
+        self.w_min = -params.w_max
         self.r_max = params.r_max
-        self.r_min = params.r_min
+        self.r_min = -params.r_max
 
         self.u_ROC_max = params.u_ROC_max * self.stepHorizon
-        self.u_ROC_min = params.u_ROC_min * self.stepHorizon
+        self.u_ROC_min = -params.u_ROC_max * self.stepHorizon
         self.v_ROC_max = params.v_ROC_max * self.stepHorizon
-        self.v_ROC_min = params.v_ROC_min * self.stepHorizon
+        self.v_ROC_min = -params.v_ROC_max * self.stepHorizon
         self.w_ROC_max = params.w_ROC_max * self.stepHorizon
-        self.w_ROC_min = params.w_ROC_min * self.stepHorizon
+        self.w_ROC_min = -params.w_ROC_max * self.stepHorizon
         self.r_ROC_max = params.r_ROC_max * self.stepHorizon
-        self.r_ROC_min = params.r_ROC_min * self.stepHorizon
+        self.r_ROC_min = -params.r_ROC_max * self.stepHorizon
 
-        self.build_solver()
+        self.activeTrajectory = params.activeTrajectory
+        self.activeTarget = params.activeTarget
 
-    def build_solver(self):
+
+        self.build_solver(self.activeTarget, self.activeTrajectory)
+
+    def next_trajectory(self, trajectory: MPCTrajectory, elapsedTime):
+        current_index = int(np.ceil((self.desiredVelocity * elapsedTime) / trajectory.resolution))
+        delta_index = int(self.desiredVelocity * self.stepHorizon / trajectory.resolution)
+        end_index = current_index + delta_index*self.N
+        max_index = (len(trajectory.x) - 1)
+        if end_index < max_index:
+            x = trajectory.x[current_index : end_index : delta_index]
+            y = trajectory.y[current_index : end_index : delta_index]
+            z = trajectory.z[current_index : end_index : delta_index]
+            psi = trajectory.psi[current_index : end_index : delta_index]
+        else:
+            x = trajectory.x[current_index : max_index : delta_index]
+            y = trajectory.y[current_index : max_index : delta_index]
+            z = trajectory.z[current_index : max_index : delta_index]
+            psi = trajectory.psi[current_index : max_index : delta_index]
+
+            # Pad with repeated last point
+            missing = (self.N - len(x))
+            x += [trajectory.x[-1]] * missing
+            y += [trajectory.y[-1]] * missing
+            z += [trajectory.z[-1]] * missing
+            psi += [trajectory.psi[-1]] * missing
+
+        self.trajectory = ca.reshape(ca.DM(np.vstack([x, y, z, psi])), -1, 1)
+
+    def build_solver(self, activeTarget, activeTrajectory):
         """
         Build the CasADi NLP solver.
 
@@ -110,10 +231,14 @@ class MPCController:
         X = ca.SX.sym('X', self.n_states, self.N + 1)
         U = ca.SX.sym('U', self.n_controls, self.N)
 
-        # Parameters: [current_state; target_state; previous_control]
-        P = ca.SX.sym('P', self.n_states * 2 + self.n_controls)
+        if activeTarget:
+            # Parameters: [current_state; previous_control; target_state]
+            P = ca.SX.sym('P', self.n_states * 2 + self.n_controls)
+        elif activeTrajectory:
+            # Parameters: [current_state; previous_control; trajectory]
+            P = ca.SX.sym('P', self.n_states + self.n_controls + self.n_states * self.N)
 
-        # Define a simple kinematic model.
+        # The kinematic model.
         psi = states[3]
         rot = ca.vertcat(
             ca.horzcat(ca.cos(psi), -ca.sin(psi), 0, 0),
@@ -126,22 +251,33 @@ class MPCController:
         # Build the cost function and dynamics constraints.
         cost_fn = 0
         g = X[:, 0] - P[0:self.n_states]  # initial state constraint
+        v_actual = P[self.n_states:self.n_states + self.n_controls]
 
         for k in range(self.N):
             st_k = X[:, k]
             con_k = U[:, k]
-            cost_fn = cost_fn \
-                      + (P[self.n_states: 2 * self.n_states] - st_k).T @ self.Q @ (P[self.n_states: 2 * self.n_states] - st_k) \
-                      + con_k.T @ self.R @ con_k
+
+            if activeTarget:
+                cost_fn = cost_fn \
+                          + (P[self.n_states + self.n_controls: 2 * self.n_states + self.n_controls] - st_k).T @ self.Q @ (P[self.n_states + self.n_controls: 2 * self.n_states + self.n_controls] - st_k) \
+                          + con_k.T @ self.R @ con_k
+            elif activeTrajectory:
+                cost_fn = cost_fn \
+                          + (P[self.n_states + self.n_controls + (self.n_states * k): self.n_states + self.n_controls + (self.n_states * (k + 1))] - st_k).T @ self.Q @ (P[self.n_states + self.n_controls + (self.n_states * k): self.n_states + self.n_controls + (self.n_states * (k + 1))] - st_k) \
+                          + con_k.T @ self.R @ con_k
+
             st_next = X[:, k + 1]
-            k1 = f(st_k, con_k)
-            k2 = f(st_k + self.stepHorizon / 2 * k1, con_k)
-            k3 = f(st_k + self.stepHorizon / 2 * k2, con_k)
-            k4 = f(st_k + self.stepHorizon * k3, con_k)
+
+            v_actual = v_actual + ( self.stepHorizon / self.tau ) * (con_k - v_actual)
+
+            k1 = f(st_k, v_actual)
+            k2 = f(st_k + self.stepHorizon / 2 * k1, v_actual)
+            k3 = f(st_k + self.stepHorizon / 2 * k2, v_actual)
+            k4 = f(st_k + self.stepHorizon * k3, v_actual)
             st_next_RK4 = st_k + (self.stepHorizon / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
             g = ca.vertcat(g, st_next - st_next_RK4)
 
-        g = ca.vertcat(g, U[:, 0] - P[2 * self.n_states: 2 * self.n_states + self.n_controls])
+        g = ca.vertcat(g, U[:, 0] - P[self.n_states: self.n_states + self.n_controls])
 
         for k in range(1, self.N):
             g = ca.vertcat(g, U[:, k] - U[:, k - 1])
@@ -225,7 +361,7 @@ class MPCController:
 
 
 
-def mpc_step(mpc: MPCController) -> tuple:
+def mpc_step(mpc: MPCController_, trajectory : MPCTrajectory) -> tuple:
     """
     Compute the optimal control using the controller solver.
 
@@ -236,7 +372,12 @@ def mpc_step(mpc: MPCController) -> tuple:
         The computed control (DM) for the current step.
     """
     # Pack parameters: current state, target state, and previous control.
-    mpc.args['p'] = ca.vertcat(mpc.state, mpc.target, mpc.u_prev)
+    if mpc.activeTrajectory:
+        mpc.next_trajectory(trajectory, mpc.elapsed_time_trajectory)
+        mpc.args['p'] = ca.vertcat(mpc.state, mpc.u_prev, mpc.trajectory)
+    elif mpc.activeTarget:
+        mpc.args['p'] = ca.vertcat(mpc.state, mpc.u_prev, mpc.target)
+
     # Pack the warm-start initial guess.
     mpc.args['x0'] = ca.vertcat(ca.reshape(mpc.X0, mpc.n_states*(mpc.N+1), 1),
                                 ca.reshape(mpc.u0, mpc.n_controls*mpc.N, 1))
@@ -252,13 +393,14 @@ def mpc_step(mpc: MPCController) -> tuple:
     u_apply = U_opt[:, 0]
 
     # Update warm-start guesses.
-    mpc.u_prev = u_apply
+    #mpc.u_prev = u_apply
     mpc.X0 = ca.horzcat(X_opt[:, 1:], X_opt[:, -1])
     mpc.u0 = ca.horzcat(U_opt[:, 1:], U_opt[:, -1])
 
     # Scale each control value by 1000 and convert to integer.
     control_values = u_apply.full().flatten() # converts the DM to a NumPy array.
     scaled_control = np.asarray((np.round(control_values * 1000, 0)),dtype="int")
+    scaled_control = np.asarray(control_values)
     u_ref, v_ref, w_ref, r_ref = scaled_control
 
     return u_ref, v_ref, w_ref, r_ref
